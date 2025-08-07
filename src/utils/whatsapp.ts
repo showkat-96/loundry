ac5ffd37659d03dc464ec73765a3c35a0eba9ae0
dev-showkat <dev.showkat.96@gmail.com>
2025-08-07 11:39:16 +0530

    whatsapp number update

--- src/utils/whatsapp.ts
+++ src/utils/whatsapp.ts
@@ -1,5 +1,5 @@
 const sendToWhatsapp = ({ message }: { message: string }) => {
-  const phone = "+9596100461";
+  const phone = "919596100461";
   const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
   window.open(url, "_blank");
 };
