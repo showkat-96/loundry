085383f161ea290351342dcfe12368ae803ffcd8
dev-showkat <dev.showkat.96@gmail.com>
2025-08-07 11:05:31 +0530

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/loundry/", // 👈 this is the key
+  base: "/",
 });
