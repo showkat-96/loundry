b3af15bc6eb02a29e03eba075190d73e836708d3
dev-showkat <dev.showkat.96@gmail.com>
2025-08-09 12:46:53 +0530

    fixed the shit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Layout from "./components/Layout";
 import ServiceDetails from "./components/ServiceDetails";
 import Home from "./components/Home";
 import DoorstepLaundry from "./components/DoorstepLaundry";
 import WhyChooseUs from "./components/WhyChooseUs";
 import OurServices from "./components/OurServices";
 import GetInTouch from "./components/GetInTouch";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
+    <>
+      <Header />
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/doorstep-loundry" element={<DoorstepLaundry />} />
         <Route path="/why-choose-us" element={<WhyChooseUs />} />
         <Route path="/our-services" element={<OurServices />} />
         <Route path="/get-in-touch" element={<GetInTouch />} />
         <Route path="/our-services/:name" element={<ServiceDetails />} />
-      </Route>
-      <Route path="*" element={<Home />} />
-    </Routes>
+        <Route path="*" element={<Home />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
